b91d523958cad6b6c0d766eb6ac776058a5fd02b
Pankaj <pankajsukhija@outlook.com>
2019-08-07 13:46:51 +0530

    removed logo.svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import TemplateRedux from './components/TemplateRedux';
+import './index.css';
+
 
 export default class App extends React.Component {
     constructor(props){
